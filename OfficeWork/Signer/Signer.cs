71af192d099e5ea80fbd1bc58ba16531c040f11a
cbt\p.sobolev <p.sobolev@cbt.by>
2019-05-11 16:26:20 +0300

    inint and add certificarte

---
+++ OfficeWork/Signer/Signer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace OfficeWork.Signer
+{
+    interface ISigner
+    {
+        
+    }
+    public class Signer : ISigner
+    {
+        private readonly X509Certificate2 certificate;
+
+        public Signer()
+        {
+            this.certificate = 
+        }
+    }
+}
